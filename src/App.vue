ad73504ff448bdc9a308603ef06a0fbc4d5f3d7c
Vicky Wang <vicky.wang01@sap.com>
2022-12-18 17:06:24 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+export default{
+  data(){
+    return {
+      activeColor: 'red',
+      fontSize: '50px',
+      bcgColor: 'pink',
+      stlyeObj: {
+        color: 'red',
+        fontSize: '100px',
+        'background-color': 'pink'
+      }
+    };
+  },
+  computed:{
+  }
+};
+</script>
+
+<!-- 绑定内联样式 -->
+<template>
+  <div>
+    <!-- 第一种：放置字符串，传统方式 -->
+    <p style="color: red">字符串</p>
+    <!-- 第二种：绑定对象 css属性名可以使用驼峰或-方式，后者需要加''引号，属性值可以用data中的变量-->
+    <p :style="{color: activeColor, fontSize:fontSize, 'background-color':bcgColor}">绑定对象</p>
+    <!-- 第三种：绑定对象 -->
+    <p :style="stlyeObj">绑定对象</p>
+    <!-- 第四种：绑定数组 -->
+    <p :style="stlyeObj">绑定对象</p>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ src/class类的多种操作.vue
@@ -0,0 +1,54 @@
+<script>
+export default{
+  data(){
+    return {
+      message: "helloWorld",
+      isActive: true,
+      error:null,
+      classObj:{
+        active:true,
+        helloWorld:true
+      }
+    };
+  },
+  computed:{
+    classObjCom(){
+      return{
+        active: this.isActive && !this.error,
+        helloWorld: this.error
+      };
+    }
+  }
+};
+</script>
+
+<template>
+  <div>
+    <!-- 第一种：放置字符串，class="类名"，类定义于style标签中 -->
+    <p class="active">hello</p>
+    <!-- 第二种：绑定对象，v-bind:class="{类名：布尔值}" -->
+    <p v-bind:class="{active:isActive}">hello绑定对象</p>
+    <p :class="{active:isActive,helloWorld:isActive}">hello绑定对象</p>
+    <button v-on:click="isActive=!isActive">改变isactive</button>
+    <!-- 和普通类一起不会被覆盖，而是合并叠加 -->
+    <p v-bind:class="{active:isActive}" class="helloWorld">hello绑定对象</p>
+    <!-- 第三种：绑定class对象，仅v-bind方式可以让class绑定对象" -->
+    <p :class="classObj">hello</p>
+    <!-- 第四种：使用computed -->
+    <p :class="classObjCom">helloComputed</p>
+     <!-- 第五种：绑定数组，不常用 -->
+     <p :class="[message]">hello数组</p>
+     <!-- 数组与对象结合使用 -->
+     <p :class="[message,{active: isActive}]">数组与对象</p>
+  </div>
+</template>
+
+<style>
+.active{
+  font-size: 50px;
+  color: blue;
+}
+.helloWorld{
+  background-color: pink;
+}
+</style>
